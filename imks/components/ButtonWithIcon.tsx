6499ef1a0cfa70a2ff800effd266481a569d27a0
Vojtaner <vojtech.laurin@gmail.com>
2024-09-19 18:20:22 +0200

    instalation of MaterialUI and moving components folder

---
+++ imks/components/ButtonWithIcon.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@mui/base/Button";
+
+export const ButtonWithIcon = () => {
+  return (
+    <>
+      <p>Ahoj</p>
+      <Button>Ahoj</Button>
+    </>
+  );
+};
--- imks/src/App.tsx
+++ imks/src/App.tsx
@@ -1,5 +1,6 @@
-function App() {
-  return <></>;
-}
+import { ButtonWithIcon } from "../components/ButtonWithIcon";
 
+function App() {
+  return <ButtonWithIcon />;
+}
 export default App;

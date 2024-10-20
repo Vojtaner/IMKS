14343f5a2c3fc13b6beefeff2fe94300073a2a37
Vojtaner <vojtech.laurin@gmail.com>
2024-10-20 17:34:23 +0200

    configure eslint, install timepicker MUI

--- imks/eslint.config.js
+++ imks/eslint.config.js
@@ -3,11 +3,24 @@ import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
 
-
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
-  {languageOptions: { globals: globals.browser }},
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+  { languageOptions: { globals: globals.browser } },
+  {
+    settings: {
+      react: {
+        version: "detect", // Automatically detect the version of React
+      },
+    },
+  },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
+  {
+    rules: {
+      semi: "error",
+      "prefer-const": "warn",
+      "react/react-in-jsx-scope": "off",
+    },
+  },
 ];

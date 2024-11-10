bb5f48b7b5598c0809fea9278b7c67a6caf02786
Vojtaner <vojtech.laurin@gmail.com>
2024-11-10 18:28:50 +0100

    WIP

--- imks/components/diagram/LightChart.tsx
+++ imks/components/diagram/LightChart.tsx
@@ -18,7 +18,7 @@ const LightChart = (props: {
       ([id, { color, legendTitle, series }]) => {
         return {
           data: series,
-          curve: "linear",
+          curve: "step",
           color,
           // zde dát pryč očekávaný null parametr jen na number
           valueFormatter: (value: number | null) => {
